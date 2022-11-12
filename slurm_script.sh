b6ed309f7f99e2bb209e4c444bb1615909c5f36e
Thea Budde <theabudde@gmail.com>
2022-11-12 16:23:43 +0100

    Use auto generated slurm

--- slurm_script.sh
+++ slurm_script.sh
@@ -1,9 +1,11 @@
-#!/usr/bin/env python
+#!/bin/bash
 
 #SBATCH -n 1
-#SBATCH --time=3-0
-#SBATCH --job-name=scaling_analysis
-#SBATCH --output=scaling_analysis.out
-#SBATCH --error=scaling_analysis.err
+#SBATCH --time=99:00:00
+#SBATCH --job-name="$0_no_field"
+#SBATCH --mem-per-cpu=1024
+#SBATCH --output="$0_no_field.out"
+#SBATCH --error="$0_no_field.err"
+#SBATCH --open-mode=truncate
 
 python main_no_field.py 2 1 10 8 "$0" 200000 ./Data
