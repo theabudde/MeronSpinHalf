3ef6d1361b65cbcb9f7fe369adc20b1172b6a747
Thea Budde <theabudde@gmail.com>
2022-11-12 15:51:26 +0100

    Add slurm script

---
+++ slurm_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#SBATCH -n 1
+#SBATCH --time=3-0
+#SBATCH --job-name=scaling_analysis
+#SBATCH --output=scaling_analysis.out
+#SBATCH --error=scaling_analysis.err
+
+python main_no_field 2 1 10 8 "$0" 200000 ./Data
