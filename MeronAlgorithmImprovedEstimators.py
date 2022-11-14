e5b61f05bc20b252269bc69ade599aa03ef49a27
Thea Budde <theabudde@gmail.com>
2022-11-14 15:07:49 +0100

    Output sign and correlation instead of quotient

--- MeronAlgorithmImprovedEstimators.py
+++ MeronAlgorithmImprovedEstimators.py
@@ -59,24 +59,19 @@ class MeronAlgorithmImprovedEstimators(MeronAlgorithm):
             self._calculate_merons()
             self.improved_two_point_function += self._two_point_functions()
             self.improved_sign += self._config_sign()
-        return self.improved_two_point_function / self.improved_sign
+        return self.improved_sign, self.improved_two_point_function
 
     def produce_data(self, U, t, beta, n, N, mc_steps, output_path):
         for i in range(1000):
             self.mc_step()
         result = []
         for i in range(100):
-            result.append(self.calculate_improved_two_point_function(mc_steps // 100))
-        result = np.array(result)
-        average = np.zeros(self.n)
-        standard_deviation = np.zeros(self.n)
-        for i in range(self.n):
-            jacknife_result = jackknife_stats(result[:, i], np.average)
-            average[i] = jacknife_result[0]
-            standard_deviation[i] = jacknife_result[2]
+            sign, two_point = self.calculate_improved_two_point_function(mc_steps // 100)
+            result.append(np.concatenate((np.array([sign]), two_point)))
 
-        data = pd.DataFrame(np.transpose(np.array([range(self.n), average, standard_deviation])),
-                            columns=['site', 'average', 'error'])
+        data = pd.DataFrame(result,
+                            columns=['sign', 'corr_0', 'corr_1', 'corr_2', 'corr_3', 'corr_4', 'corr_5', 'corr_6',
+                                     'corr_7'])
         data.to_csv(os.path.join(output_path,
                                  f'correlation_function_U={U}_t={t}_beta={beta}_L={n}_T={N}_mcsteps={mc_steps}.csv'))
 
--- slurm_script.sh
+++ slurm_script.sh
@@ -2,10 +2,10 @@
 
 #SBATCH -n 1
 #SBATCH --time=99:00:00
-#SBATCH --job-name="$0_no_field"
+#SBATCH --job-name="1000_no_field"
 #SBATCH --mem-per-cpu=1024
-#SBATCH --output="$0_no_field.out"
-#SBATCH --error="$0_no_field.err"
+#SBATCH --output="./output/%j.out"
+#SBATCH --error="./output/%j.err"
 #SBATCH --open-mode=truncate
 
-python ./main_no_field.py 2 1 10 8 "$0" 200000 ./Data
+python ./main_no_field.py 2 1 1 8 1000 100000 /cluster/home/tbudde/MeronSpinHalf/Data
