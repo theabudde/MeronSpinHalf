3577455b66425e90ee207ad5c1cf1466d98b46bf
Thea Budde <theabudde@gmail.com>
2022-12-05 13:47:42 +0100

    Add a brute force Spin Half version

---
+++ SpinHalfBruteForce.py
@@ -0,0 +1,61 @@
+import numpy as np
+from MeronAlgorithm import MeronAlgorithm
+
+
+class SpinHalfBruteForce(MeronAlgorithm):
+
+    def __init__(self, n, t, w_a, w_b, mc_steps):
+        MeronAlgorithm.__init__(self, n, t, w_a, w_b, mc_steps)
+        self.gauge_field = np.array([])
+        self.result = np.zeros(n)
+
+    def generate_flips(self):
+        result = np.zeros(self.n)
+        n_legal_configs = 0
+        for flip in range(2 ** self.n_clusters):
+            self.flip = flip
+            self.fermion = np.full((self.n, self.t), False)
+            for i in range(self.n // 2):
+                for j in range(self.t):
+                    self.fermion[2 * i, j] = True
+            self._flip()
+            self._calculate_gauge_field()
+            if self._test_gauss_law():
+                n_legal_configs += 1
+                for site in range(self.n):
+                    if self.fermion[0, 0] == self.fermion[site, 0]:
+                        result[site] += 1
+                    else:
+                        result[site] -= 1
+        self.result += result / n_legal_configs
+
+    def _calculate_gauge_field(self):
+        self.gauge_field = np.zeros(self.n_clusters)
+        for x in range(self.n - 1):
+            for y in range(1, self.t):
+                if (y + 1) % 2 != x % 2 or self.fermion[x, y] == self.fermion[x, y - 1]:
+                    self.gauge_field[x, y] = self.gauge_field[x, y - 1]
+                elif not self.fermion[x, y]:
+                    self.gauge_field[x, y] = self.gauge_field[x, y - 1] - 1
+                else:
+                    self.gauge_field[x, y] = self.gauge_field[x, y - 1] + 1
+            if self.fermion[x + 1, 0] == x % 2:
+                self.gauge_field[x + 1, 0] = self.gauge_field[x, 0]
+            elif x % 2:
+                self.gauge_field[x + 1, 0] = self.gauge_field[x, 0] - 1
+            else:
+                self.gauge_field[x + 1, 0] = self.gauge_field[x, 0] + 1
+
+    def _test_gauss_law(self):
+        if np.amax(self.gauge_field) - np.amin(self.gauge_field) > 1:
+            return False
+        return True
+
+    def corr_function(self):
+        self._assign_bonds()
+        self._reset()
+        self._find_clusters()
+        for i in range(self.mc_steps):
+            self.mc_step()
+            self.generate_flips()
+        self.result /= self.mc_steps
---
+++ main_brute_force.py
@@ -0,0 +1,54 @@
+import os
+
+import numpy as np
+import pandas as pd
+
+from SpinHalfBruteForce import SpinHalfBruteForce
+import sys
+
+
+def main(argv):
+    # argv parameters: U, t, beta, lattice_width, time_steps, mc_steps, result_path, job_array_id
+    U = float(argv[1])
+    t = float(argv[2])
+    beta = float(argv[3])
+    lattice_width = int(argv[4])
+    if lattice_width % 2 != 0:
+        raise ValueError('lattice can not have an odd number of sites')
+    if lattice_width < 4:
+        raise ValueError('Algorithm only works for grid sizes 4 and higher')
+    time_steps = int(argv[5])
+    mc_steps = int(argv[6])
+    if mc_steps % 10 != 0:
+        raise ValueError('error can only be calculated if mc steps is a multiple of 10')
+    result_path = argv[7]
+    job_array_nr = argv[8]
+
+    eps = beta / time_steps
+    if eps > 0.15:
+        return
+
+    w_a = np.exp(- eps * U / 4)
+    w_b = np.exp(eps * U / 4) * np.sinh(eps * t)
+
+    # Initialise to reference configuation
+    algorithm = SpinHalfBruteForce(lattice_width, 2 * time_steps, w_a, w_b, mc_steps)
+
+    # Thermalise
+    for i in range(1000):
+        algorithm.mc_step()
+
+    # calculate two point function
+    algorithm.corr_function()
+
+    # output result to csv file
+    output_path = os.path.join(result_path,
+                               f'correlation_function_U={U}_t={t}_beta={beta}_L={lattice_width}_T={time_steps}.csv')
+    columns = np.array(['n_steps'] + [f'corr_{i}' for i in range(lattice_width)])
+    data = np.concatenate((np.array([mc_steps]), algorithm.result))
+    data = pd.DataFrame(np.array([data]), columns=columns)
+    data.to_csv(output_path, mode='a', header=not os.path.exists(output_path))
+
+
+if __name__ == "__main__":
+    main(sys.argv)
